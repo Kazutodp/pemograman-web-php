1dae7c9761c017c5cf7c54101857b5bd1e37d026
Kazutodp <exsokaneki@gmail.com>
2026-04-13 12:11:21 +0800

    versi 1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama = "abdul";
+$umur = 20;
+$tinggi = 170.5;
+$menikah = true;
+$hobi = ["membaca", "bermain gitar", "bermain gitar"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi, status saya $menikah, hobi saya $hobi[0],  $hobi[1]";
+
+?>
