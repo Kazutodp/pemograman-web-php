9258d7dcd1e244f1489cb632d51244e9185f35c2
Kazutodp <exsokaneki@gmail.com>
2026-05-09 03:42:53 +0800

    penambahan fitur hapus

--- materi4.php
+++ materi4.php
@@ -17,6 +17,42 @@ if (isset($_POST['kirim'])) {
 }
 ?>
 
+//menampilkan data
+<table border="1">
+    <th>id</th>
+    <th>Username</th>
+    <th>Password</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>aksi</th>
+
+    <?php
+    $sql = "SELECT * FROM user";
+    $query = mysqli_query($koneksi, $sql);
+    while ($row = mysqli_fetch_assoc($query)) {
+        echo"<tr>";
+        echo"<td>" . $row['id'] . "</td>";
+        echo"<td>" . $row['username'] . "</td>";
+        echo"<td>" . $row['password'] . "</td>";
+        echo"<td>" . $row['nama'] . "</td>";
+        echo"<td>" . $row['email'] . "</td>";
+        echo"<td> <a href='materi4.php?edit=$row[id]'>Edit</a> | <a href='materi4.php?hapus=$row[id]'>Hapus</a> </td>";
+        echo"</tr>";
+    }
+    if (isset($_GET['hapus'])) {
+        $id = $_GET['hapus'];
+        $sql = "DELETE FROM user WHERE id = $id";
+        $query = mysqli_query($koneksi, $sql);
+        if ($query) {
+            echo "Data berhasil dihapus";
+        } else {
+            echo "Data gagal dihapus";
+        }
+    }
+    ?> 
+</table>
+
+
 <form method = "POST">
     Username : <input type="text" name="username"><br><br>
     password : <input type="password" name="password"><br><br>
